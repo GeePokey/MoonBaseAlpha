ddd78ecdff540f8f21d9d6e92ad27024359b471a
Dakota Bullis <dbullis@gmail.com>
2020-11-29 18:10:16 -0800

    Update D4

--- AOC2019/D4.py
+++ AOC2019/D4.py
@@ -1,4 +1,5 @@
 
+
 inputstart, inputend = 109165,576723
 
 def producePossiblePWs(startrange,endrange):
@@ -11,13 +12,11 @@ def producePossiblePWs(startrange,endrange):
 
 def neverDecrease(num):
     #Left to right digits never decrease
-    pointer = 0
     strnum = str(num)
     for i in range(len(strnum)-1):
-        if pointer > len(strnum)+1:
+        if i > len(strnum)+1:
             break
-        if int(strnum[pointer]) <= int(strnum[pointer+1]):
-            pointer += 1
+        if int(strnum[i]) <= int(strnum[i+1]):
             continue
         else:
             return False
@@ -30,15 +29,12 @@ assert neverDecrease(923781) == False
 
 
 def minTwoAdjacent(num):
-    pointer = 0
     strnum = str(num)
     for i in range(len(strnum)-1): #might be 1 off maybe -1
-        if pointer > len(strnum)+1:
+        if i > len(strnum)+1:
             return False
-        if strnum[pointer] == strnum[pointer+1]:
+        if strnum[i] == strnum[i+1]:
             return True
-        else:
-            pointer += 1
     return False
 
 assert (minTwoAdjacent(121211)) == True
@@ -47,4 +43,34 @@ assert (minTwoAdjacent(1212212)) == True
 assert (minTwoAdjacent(121212)) == False
 assert (minTwoAdjacent(123789)) == False
 
-print(len(producePossiblePWs(inputstart,inputend)))
+# print(len(producePossiblePWs(inputstart,inputend)))
+
+
+#part 2
+def p2producePossiblePWs(startrange,endrange):
+    possiblepws = []
+    for i in range(startrange,endrange):#endrange +1?
+        if neverDecrease(i):
+            if ONLYTWO(i):
+                possiblepws.append(i)
+    return possiblepws
+
+def ONLYTWO(num):
+    strnum = str(num)
+    for i in range(len(strnum)-1):
+        if i+2 > len(strnum)-1:
+            if strnum[i] == strnum[i+1] and strnum[i] > strnum[i-1]:
+                return True
+            return False
+        if strnum[i] == strnum[i+1] and strnum[i] < strnum[i+2]:
+            return True
+
+#number needs ONLY ONE pair that isnt part of a larger group
+assert (ONLYTWO(112233)) == True
+assert (ONLYTWO(123444)) == False
+assert (ONLYTWO(111122)) == True
+assert (ONLYTWO(111122)) == True
+assert (ONLYTWO(111122)) == True
+
+
+print(len(p2producePossiblePWs(inputstart,inputend)))
