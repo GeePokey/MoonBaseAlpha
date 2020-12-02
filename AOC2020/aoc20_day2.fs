3689f854b67188ded274ad7e3a474fab73783e2f
Gumby <geepokey@gmail.com>
2020-12-01 22:58:54 -0800

    snapshot day2

--- AOC2020/aoc20_day2.fs
+++ AOC2020/aoc20_day2.fs
@@ -9,42 +9,19 @@ let ra = seq { for i in lines do
                    let fields = i.Split(" ")
                    let rrange = fields.[0].Split("-")
                    yield { rmin = (System.Int32.Parse rrange.[0]) ;
-                           rmax = (System.Int32.Parse rrange.[1] );
+                           rmax = (System.Int32.Parse rrange.[1]) ;
                            rletter = fields.[1].[0];
-                           rpassword = fields.[2]  }
-               }
-//     printfn "%A %A %A" (fields.[0].Split("-")) fields.[1] fields.[2]
+                           rpassword = fields.[2]  } }
 
 let is_valid (case1: Rule) =
     let rcount = case1.rpassword |> Seq.filter (fun c -> c=case1.rletter) |> Seq.length
     (rcount >= case1.rmin ) && (rcount <= case1.rmax )
 
-let xis_valid2 (case1: Rule) =
-    let x = (case1.rmin - 1)
-    let y = (case1.rmax - 1)
-    let len = (String.length case1.rpassword)
-    if len < x
-    then false
-    else if case1.rpassword.[x] = case1.rletter
-         then if len < y || not (case1.rpassword.[y] = case1.rletter)
-              then true
-              else not (case1.rpassword.[y] = case1.rletter)
-         else if len < y || (case1.rpassword.[y] = case1.rletter)
-              then false
-              else true
-
 let is_valid2 (case1: Rule) =
-    let x = (case1.rmin - 1)
-    let y = (case1.rmax - 1)
-    let len = (String.length case1.rpassword)
-    if len < x
-    then false
-    else if case1.rpassword.[x] = case1.rletter
-         then not (case1.rpassword.[y] = case1.rletter)
-         else (case1.rpassword.[y] = case1.rletter)
+    (if case1.rpassword.[case1.rmin - 1] = case1.rletter then not else id )
+           (case1.rpassword.[case1.rmax - 1] = case1.rletter)
 
+// for j in ra do printfn "%A %A" j (is_valid2 j)
 
-for j in ra do printfn "%A %A" j (is_valid2 j)
-
-printfn "Part1 %d " ( ra |> Seq.filter is_valid |> Seq.length )
+printfn "Part1 %d " ( ra |> Seq.filter is_valid  |> Seq.length )
 printfn "Part1 %d " ( ra |> Seq.filter is_valid2 |> Seq.length )
