7d765c5863b605f5d5c34bcf247bf8565ce2e797
Gumby <geepokey@gmail.com>
2020-12-03 23:31:43 -0800

    deleted bad tries - just left good stuff.

--- AOC2020/aoc20_day4.fs
+++ AOC2020/aoc20_day4.fs
@@ -3,7 +3,7 @@ printfn "Advent of Code 2020 Day 4"
 let lines = System.IO.File.ReadAllLines("input.day4.txt") 
 // let lines = System.IO.File.ReadAllLines("input.txt")
 // let lines = System.IO.File.ReadAllLines("part2.txt") 
-printfn "%A" lines
+// printfn "%A" lines
 
 let len = lines |> Seq.length
 let width = lines.[0] |> Seq.length
@@ -67,56 +67,6 @@ Compilation finished at Thu Dec  3 21:58:27
 
 // Part 2
 
-// // byr (Birth Year) - four digits; at least 1920 and at most 2002.
-// // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-// // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-// // hgt (Height) - a number followed by either cm or in:
-// // If cm, the number must be at least 150 and at most 193.
-// // If in, the number must be at least 59 and at most 76.
-// // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-// // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-// // pid (Passport ID) - a nine-digit number, including leading zeroes.
-// // cid (Country ID) - ignored, missing or not.
-
-// let rec monovalidate2 (s:string) =
-//      // printfn " monovalidate %A %A %A " s ( s.Contains("byr:") )      (if s.Contains "byr:" then 0x02 else 0 )
-//     if s.Contains " "
-//     then s.Split(" ") |> Seq.map monovalidate2 |> Seq.sum
-//     else
-//         let fields = s.Split(":")
-//         let token = fields.[0]
-//         let value = fields.[1]
-           
-//         //         (if token.Contains "cid" then 0x00 else 0 ) |||
-//         (if token.Contains "byr"
-//                     then match System.Int32.TryParse value with
-//                           | true,x -> if x >= 1920 && x <= 2002 then 2 else 0
-//                           | _ -> 0
-//                     else 0 ) ||| 
-//         (if token.Contains "iyr"
-//                 then 
-//                   match System.Int32.TryParse value with
-//                   | true,x -> if x >= 2010 && x <= 2020 then 0x04 else 0
-//                   | _ -> 0
-//                 else 0 ) ||| 
-//         (if token.Contains "eyr"
-//                   match System.Int32.TryParse value with
-//                   | true,x -> if x >= 2020 && x <= 2030 then 0x08 else 0
-//                   | _ -> 0
-//              else 0 ) ||| 
-//         (if token.Contains "hgt"
-//              then 0x10
-//              else 0 ) ||| 
-//         (if token.Contains "hcl"
-//              then 0x20
-//              else 0 ) ||| 
-//         (if token.Contains "ecl"
-//              then 0x40
-//              else 0 ) ||| 
-//         (if token.Contains "pid"
-//              then 0x80
-//              else 0 ) 
-
 open System.Text.RegularExpressions
 
 let (|Regex|_|) pattern input =
@@ -171,70 +121,13 @@ let rec monovalidate2 (s:string) =
                                              | "grn" -> 0x40
                                              | "hzl" -> 0x40
                                              | "oth" -> 0x40
-                                             | _ -> printfn "no match: %A %A" ma s
+                                             | _ -> // printfn "no match: %A %A" ma s
                                                     0
             | Regex @"pid:([\d]{9})$"  [ma] -> 0x80
-            | _  -> printfn "no match: %A " s
+            | _  -> // printfn "no match: %A " s
                     0 // Regex @"cid:.*" -> 0
 
-            
-        // let fields = s.Split(":")
-        // let token = fields.[0]
-        // let value = fields.[1]
-           
-        // //         (if token.Contains "cid" then 0x00 else 0 ) |||
-        // (if token.Contains "byr"
-        //             then match System.Int32.TryParse value with
-        //                   | true,x -> if x >= 1920 && x <= 2002 then 2 else 0
-        //                   | _ -> 0
-        //             else 0 ) ||| 
-        // (if token.Contains "iyr"
-        //         then 
-        //           match System.Int32.TryParse value with
-        //           | true,x -> if x >= 2010 && x <= 2020 then 0x04 else 0
-        //           | _ -> 0
-        //         else 0 ) ||| 
-        // (if token.Contains "eyr"
-        //           match System.Int32.TryParse value with
-        //           | true,x -> if x >= 2020 && x <= 2030 then 0x08 else 0
-        //           | _ -> 0
-        //      else 0 ) ||| 
-        // (if token.Contains "hgt"
-        //      then 0x10
-        //      else 0 ) ||| 
-        // (if token.Contains "hcl"
-        //      then 0x20
-        //      else 0 ) ||| 
-        // (if token.Contains "ecl"
-        //      then 0x40
-        //      else 0 ) ||| 
-        // (if token.Contains "pid"
-        //      then 0x80
-        //      else 0 ) 
-    
 
-
-let rec sub_validate2 passport accum =
-   match passport with
-       | h::tail  -> // printfn "sub validate %A" h
-                     sub_validate2 tail (accum  ||| (monovalidate2 h))
-       | []  -> printfn "end of passport 0x%02X %s " accum ( if (accum &&& 0xFE) = 0xFE then "2good" else "2bad" )
-                accum
-   accum
-
- 
-let rec validate2 input paccum =
-    match input with
-        | ""::tail -> sub_validate2 paccum 0 
-                      validate2 tail []
-                      // printfn "a blank line"
-                      ()
-        | h::tail  -> validate2 tail (h::paccum)
-                      // printfn "a passport line: %A " h
-                      ()
-        
-        | []       -> printfn "end of list"
-        
 
 // Part1
 printfn "Part1 %d " ( validate1 ( lines |> Seq.toList ) ["fred"] 0 monovalidate)
@@ -243,4 +136,11 @@ printfn "Part1 %d " ( validate1 ( lines |> Seq.toList ) ["fred"] 0 monovalidate)
 printfn "Part2 %d " ( validate1 ( lines |> Seq.toList ) ["fred"] 0 monovalidate2)
 
 
-validate2 ( lines |> Seq.toList ) ["fred"] 
+(*
+          
+Part1 245 
+Part2 133 
+        0.26 real         0.16 user         0.01 sys
+
+Compilation finished at Thu Dec  3 23:29:08
+*)
