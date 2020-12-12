6df778b7fbc9f9169fb7b97e75f565c3f8826bc7
Gumby <geepokey@gmail.com>
2020-12-11 22:31:09 -0800

    part2 dad

--- AOC2020/11.fs
+++ AOC2020/11.fs
@@ -2,9 +2,9 @@ printfn "Advent of Code 2020 Day 11"
 // Day 11
 
 
-//let lines = System.IO.File.ReadAllLines("input.day11.txt")
+let lines1 = System.IO.File.ReadAllLines("input.day11.txt")
 
-let lines1 = System.IO.File.ReadAllLines("sample.day11.txt")
+// let lines1 = System.IO.File.ReadAllLines("sample.day11.txt")
 // let lines =  Array.map int lines1 
 
 // let lines = lines1 |> Array.map (fun c -> c.ToCharArray() )
@@ -51,9 +51,24 @@ let neighboors data x y =
     neighbooroffsets |> Seq.sumBy (fun (dx,dy) -> if (read_cell data ( x + dx )  ( y + dy )) = OCCUPIED then 1 else 0 )
 
 
+// Part 2
+let read_cell_radially ( data : char [,] ) ax ay dx dy =
+    let mutable x = ax + dx
+    let mutable y = ay + dy
+//    while x > 1 && y > 1 && x < (width-1) && y < (height-1) && data.[y,x] =  FLOOR do
+    while x >= 0 && y >= 0 && x < width && y < height && data.[y,x] = FLOOR do
+        x <- x + dx
+        y <- y + dy
+    if x >= 0 && y >= 0 && x < width && y < height then data.[y,x] else FLOOR
 
-let dump ( (grid: char [,] ) , modflag ) =
-    printfn "Mod: %A" modflag
+
+// Part 2        
+let neighboors_part2 data x y =
+    neighbooroffsets |> Seq.sumBy (fun (dx,dy) -> if (read_cell_radially data  x  y dx dy) = OCCUPIED then 1 else 0 )
+
+
+let dump ( (grid: char [,] ) , modflag, counti ) =
+    printfn "Mod: %A count %d" modflag counti
     for row in [0..height-1] do
         for col in [0..width-1] do 
            printf "%c" grid.[row,col] 
@@ -67,34 +82,46 @@ If a seat is occupied (#) and four or more seats adjacent to it are also occupie
 Otherwise, the seat's state does not change.
              *)
              
-let update (data : char [,] )  =
+let updatex neighboorpartx ocx (data : char [,]  ) :  char [,] * bool * int =
     let mutable dest = Array2D.create  height width FLOOR
     // dump dest
     let mutable modified = false
+    let mutable modcount = 0
 
 
     for y in [0.. height - 1 ] do
         for x in [0.. width - 1 ] do
-            match (read_cell data x y ), (neighboors data x y) with
+            match (read_cell data x y ), (neighboorpartx data x y) with
                 | FLOOR, _    -> ()  // dest.[y,x] <- FLOOR
-                | OCCUPIED, oc when oc >= 4 -> dest.[y,x] <- CHAIR
-                                               modified <- true
-                                               // printfn "E: %d,%d" x y 
+                | OCCUPIED, oc when oc >= ocx -> dest.[y,x] <- CHAIR
+                                                 modified <- true
+                                                 modcount <- modcount + 1
+                                                 // printfn "E: %d,%d" x y 
                 | CHAIR, 0    -> dest.[y,x] <- OCCUPIED
                                  modified <- true
+                                 modcount <- modcount + 1                                 
                                  // printfn "O: %d,%d" x y
                 | state,cc     -> dest.[y,x] <- state
                                   // printfn "S: %d,%d = %d" x y cc
-    dest, modified
+    dest, modified , modcount
+
+
+// let  update = updatex neighboors 4
+let  update = updatex neighboors_part2 5
+
+let third (_, _, c) = c
+let my2 (_, c, _ ) = c
+let my1 (c, _, _) = c
+
 
 printfn "Main"
-dump (lines, false)
-let mutable x = lines,true
+dump (lines, false, 0)
+let mutable x =  lines, true, 0
 let mutable passes = 0
-while (snd x) do
+while (my2 x) do
     passes <- passes + 1
-    // printfn "\npass %d\n" passes
-    x <- update (fst x)
+    x <- update (my1 x)
+    printfn "\npass %5d %4d %A\n" passes (third x) (my2 x)
     // dump x
 
 let countdump test (grid: char [,] ) =
@@ -106,7 +133,7 @@ let countdump test (grid: char [,] ) =
 }
 
 dump x
-printfn "Part 1 passes %d count occupied %d " passes ((fst x) |> countdump (fun c ->  c = OCCUPIED) |> Seq.length )
+printfn "Part 2 passes %d count occupied %d " passes ((my1 x) |> countdump (fun c ->  c = OCCUPIED) |> Seq.length )
 
 
 (*   
@@ -144,4 +171,36 @@ Part 1 passes 6 count occupied 37
 
 Compilation finished at Thu Dec 10 23:51:28
 
+Part 1 passes 95 count occupied 2424 
+        1.69 real         1.05 user         0.04 sys
+
+Compilation finished at Thu Dec 10 23:57:00
+
+*)
+
+(*
+ part 2
+That's not the right answer; your answer is too high. If you're stuck, make sure you're using the full input data; there are also some general tips on the about page, or you can ask for hints on the subreddit. Please wait one minute before trying again. (You guessed 2330.) [Return to Day 11]
+
+
+Part 1 passes 88 count occupied 2330 
+        2.28 real         1.17 user         0.05 sys
+
+Compilation finished at Fri Dec 11 22:13:07
+
+
+
+#.LLLLL#.L
+#.L#LL#.L#
+Part 2 passes 7 count occupied 26  // correct answer for sample part 2!
+        0.18 real         0.14 user         0.01 sys
+
+Compilation finished at Fri Dec 11 22:25:27
+
+
+L#L#L#L.#L#L#
+Part 2 passes 89 count occupied 2208 
+        2.14 real         1.21 user         0.05 sys
+
+Compilation finished at Fri Dec 11 22:26:56
 *)
